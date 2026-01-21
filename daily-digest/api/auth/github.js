0f7f77c1710ba458802af78ed0909464b46466f3
ashleyHutton <ashley@brandnewbox.com>
2026-01-21 08:42:01 -0600

    Add user:email scope to GitHub OAuth

--- daily-digest/api/auth/github.js
+++ daily-digest/api/auth/github.js
@@ -2,7 +2,7 @@
 module.exports = async (req, res) => {
   const clientId = process.env.GITHUB_CLIENT_ID;
   const redirectUri = `${process.env.APP_URL}/api/auth/github/callback`;
-  const scope = 'read:user repo';
+  const scope = 'read:user user:email repo';
 
   const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
 
